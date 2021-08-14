e24bb7b9fc53b5c13fa0da8e1f4589cf95dff46e
SnO2WMaN <me@sno2wman.net>
2021-08-15 06:12:28 +0900

    test修正

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,4 @@
 import type {Config} from '@jest/types';
-import {pathsToModuleNameMapper} from 'ts-jest/utils';
-
-import {compilerOptions} from './tsconfig.json';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
@@ -9,9 +6,6 @@ const config: Config.InitialOptions = {
   testEnvironment: 'node',
   rootDir: './',
   testMatch: ['<rootDir>/src/**/test/*.test.ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
   collectCoverage: true,
   coverageDirectory: './coverage',
   reporters: ['default'],
